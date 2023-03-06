a6a82eb9eeda28f941940de5f9e0fab69acac7ae
Keval Padsala <47670635+Keval78@users.noreply.github.com>
2023-03-06 14:16:06 -0500

    Leetcode Daily

--- LeetCode/Daily/974 Subarray Sums Divisible by K.py
+++ LeetCode/Daily/974 Subarray Sums Divisible by K.py
@@ -7,19 +7,17 @@ Leetcode: https://leetcode.com/Keval_78/
 
 def main():
     class Solution:
-        # NEED TO WORK
         def subarraysDivByK(self, nums: List[int], k: int) -> int:
             n_pairs = 0
-        
-            prefix = nums.copy()
-            prefix[1:] = [prefix[i-1] + nums[i] for i in range(1, len(nums))]
-            print(prefix)
+            prefix_mod = 0 
 
-            for i in range(len(nums)-1):
-                for j in range(i, len(nums)):
-                    if prefix[i]%k==prefix[j]%k:
-                        n_pairs+=1
-
+            mod_groups = [0]*k
+            mod_groups[0] = 1
+            
+            for i in range(len(nums)):
+                prefix_mod = ((prefix_mod+nums[i])%k+k)%k
+                n_pairs += mod_groups[prefix_mod]
+                mod_groups[prefix_mod]+=1
             return n_pairs
             
     Solution().subarraysDivByK()
