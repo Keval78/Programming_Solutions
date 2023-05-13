31988b90ddf592e54aab4094e99f13703fac0d5d
Keval78 <kevalpadashala@gmail.com>
2023-05-13 19:28:02 -0400

    HE May DSA '23

---
+++ Hackerearth/May DSA '23/Split String.py
@@ -0,0 +1,125 @@
+from __future__ import division, print_function
+from cmath import inf
+
+import os
+import sys
+from io import BytesIO, IOBase
+from collections import defaultdict, Counter
+
+
+def ii(): return int(input())
+def si(): return input()
+def mi(ss=" "): return map(int, input().strip().split(ss))
+def msi(ss=" "): return map(str, input().strip().split(ss))
+def li(ss=" "): return list(mi(ss))
+
+
+def read():
+    sys.stdin = open('./input.txt', 'r')
+    sys.stdout = open('./output.txt', 'w')
+
+
+def main():
+    for _ in range(ii()):
+        n, k = mi()
+        s = si()
+        freq = Counter(s)
+        cond1, cond2 = False, False
+        for val in freq.values():
+            if val >= k:
+                cond1 = True
+        if len(freq.keys()) >= k:
+            cond2 = True
+
+        if cond1 and cond2:
+            print("YES")
+        else:
+            print("NO")
+
+    return
+
+
+# region fastio
+# template taken from https://github.com/cheran-senthil/PyRival/blob/master/templates/template.py
+BUFSIZE = 8192
+
+
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+
+
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+
+
+def print(*args, **kwargs):
+    """Prints the values to a stream, or to sys.stdout by default."""
+    sep, file = kwargs.pop("sep", " "), kwargs.pop("file", sys.stdout)
+    at_start = True
+    for x in args:
+        if not at_start:
+            file.write(sep)
+        file.write(str(x))
+        at_start = False
+    file.write(kwargs.pop("end", "\n"))
+    if kwargs.pop("flush", False):
+        file.flush()
+
+
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+def input(): return sys.stdin.readline().rstrip("\r\n")
+
+
+# endregion
+
+if __name__ == "__main__":
+    # read()
+    main()
+
+
+# * PROBLEM DESCERIPTION
+# link :- https://www.hackerearth.com/practice/algorithms/greedy/basics-of-greedy-algorithms/practice-problems/algorithm/split-string-2-f088aa39/
+# Test Input
+'''
+2
+3 4 
+xyz
+5 3
+abaca
+'''
