6583b5372e799d395457a4b491be427a89d39ebf
Keval Padsala <47670635+Keval78@users.noreply.github.com>
2023-06-14 02:46:44 -0400

    Leetcode Daily

--- LeetCode/Daily/73 Set Matrix Zeroes.py
+++ LeetCode/Daily/73 Set Matrix Zeroes.py
@@ -20,17 +20,18 @@ def main():
                         matrix[i][0] = 0
                         if i==0: first_row_ele = True
                         if j==0: first_col_ele = True
+            
+            # start setting elements zero.
             for i in range(1, len(matrix)):
-                if matrix[i][0] == 0:
-                    for j in range(len(matrix[i])):
+                for j in range(1, len(matrix[i])):
+                    if matrix[i][0] == 0 or matrix[0][j] == 0:
                         matrix[i][j] = 0
-            for j in range(1, len(matrix[0])):
-                if matrix[0][j] == 0:
-                    for i in range(len(matrix)):
-                        matrix[i][j] = 0
+            
+            # See if the first row needs to be set to zero as well
             if first_col_ele:
                 for i in range(len(matrix)):
                     matrix[i][0] = 0
+            # See if the first column needs to be set to zero as well
             if first_row_ele:
                 for j in range(len(matrix[0])):
                     matrix[0][j] = 0
