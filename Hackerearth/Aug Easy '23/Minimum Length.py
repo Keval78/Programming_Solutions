f34c187a9e00b1e623f288904bafafff476dd250
Keval Padsala <47670635+Keval78@users.noreply.github.com>
2023-08-08 00:45:53 -0400

    HE Easy Aug'23

---
+++ Hackerearth/Aug Easy '23/Minimum Length.py
@@ -0,0 +1,40 @@
+'''
+###### * User Profile : Keval_78
+LinkedIn: https://www.linkedin.com/in/kevalpadsala78/
+Github: https://github.com/Keval78
+Leetcode: https://leetcode.com/Keval_78/
+'''
+ 
+import sys
+import operator
+# from os import path
+# from math import log2, floor, ceil, sqrt, pow, gcd
+# from random import random, randint, shuffle, choice
+# from collections import Counter, defaultdict, deque
+# from itertools import permutations, combinations
+# from functools import reduce
+# from heapq import heapify, heappop, heappush, heapreplace
+from bisect import bisect, bisect_left, bisect_right
+
+ 
+def si(): return sys.stdin.readline().strip()
+def ii(): return int(si())
+def mi(ss=" "): return map(int, si().split(ss))
+def msi(ss=" "): return map(str, si().split(ss))
+def li(ss=" "): return list(mi(ss))
+ 
+sys.setrecursionlimit(10 ** 5)
+MOD, MOD2, INF = 10 ** 9 + 7, 998244353, float('inf')
+MAX = 10**5+1
+
+
+
+for _ in range(ii()):
+    n = ii()
+    arr = li()
+    brr = li()
+    left, right = 0, n-1
+    while arr[left] == brr[left]: left+=1
+    while arr[right] == brr[right]: right-=1
+    ans = max(0, right-left+1)
+    print(ans)
