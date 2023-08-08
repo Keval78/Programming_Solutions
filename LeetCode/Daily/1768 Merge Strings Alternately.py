2a71c93f58b089ddffe7a2dcf11ef2bc606860e2
Keval78 <kevalpadashala@gmail.com>
2023-08-07 23:43:05 -0400

    Leetcode Daily

--- LeetCode/Daily/1768 Merge Strings Alternately.py
+++ LeetCode/Daily/1768 Merge Strings Alternately.py
@@ -5,24 +5,25 @@ Github: https://github.com/Keval78
 Leetcode: https://leetcode.com/Keval_78/
 '''
 
+
 def main():
     class Solution:
         def mergeAlternately(self, word1: str, word2: str) -> str:
             merged = ""
             for i in range(min(len(word1), len(word2))):
                 merged += f"{word1[i]}{word2[i]}"
-            
+
             for j in range(i+1, len(word1)):
                 merged += f"{word1[j]}"
             for j in range(i+1, len(word2)):
                 merged += f"{word2[j]}"
-            
+
             # for i,j in zip_longest(word1,word2, fillvalue=''):
             #     merged += f"{i}{j}"
 
             return merged
-            
-    Solution().mergeAlternately()
+
+    # Solution().mergeAlternately()
 
 
 if __name__ == "__main__":
