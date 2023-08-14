e2e3670e8e0037460dc95a18ee73c1a42d428ea2
Keval78 <kevalpadashala@gmail.com>
2023-08-13 21:47:06 -0400

    DisjointSet CC

--- Data Structures & Alogrithms/Data Structures/disjoint_set_cc.py
+++ Data Structures & Alogrithms/Data Structures/disjoint_set_cc.py
@@ -8,15 +8,27 @@ Leetcode: https://leetcode.com/Keval_78/
 
 class DisjointSet:
     def __init__(self, n):
-        self.rank = [0]*n
         self.parent = [i for i in range(n)]
+        self.size = [1]*n
+        self.rank = [0]*n
 
     def find(self, x):
         if x != self.parent[x]:
             self.parent[x] = self.find(self.parent[x])
         return self.parent[x]
 
-    def union(self, x, y):
+    def union_size(self, x, y):
+        x, y = self.find(x), self.find(y)
+        if x == y:
+            return
+        if self.size[x] < self.size[y]:
+            self.parent[x] = y
+            self.size[y] += self.size[x]
+        else:
+            self.parent[y] = x
+            self.size[x] += self.size[y]
+
+    def union_rank(self, x, y):
         x, y = self.find(x), self.find(y)
         if x == y:
             return
