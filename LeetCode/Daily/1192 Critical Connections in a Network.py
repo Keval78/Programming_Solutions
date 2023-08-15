f6848fa4b53565f01396ff1c7ec7c8ccef437621
Keval Padsala <47670635+Keval78@users.noreply.github.com>
2023-08-15 01:47:57 -0400

    Leetcode Daily

---
+++ LeetCode/Daily/1192 Critical Connections in a Network.py
@@ -0,0 +1,35 @@
+class Solution:
+    def dfs(self, node, parent, graph, tin, low, bridges):
+        # Define intime and low
+        self.timer += 1
+        tin[node] = low[node] = self.timer
+
+        for adj in graph[node]:
+            if adj == parent: continue
+            if tin[adj] == 0:
+                self.dfs(adj, node, graph, tin, low, bridges)
+                low[node] = min(low[node], low[adj])
+                if low[adj] > tin[node]:
+                    bridges.append([node, adj])
+            else:
+                low[node] = min(low[node], low[adj])
+
+
+    def criticalConnections(self, n: int, connections: List[List[int]]) -> List[List[int]]:
+        # Tarjan's Algorithm
+        graph = [[] for i in range(n)]
+        for u, v in connections:
+            graph[u].append(v)
+            graph[v].append(u)
+        
+
+        # Find Bridges for the graph
+        self.timer = 0 # Create timer for each dfs call.
+        bridges= []
+
+        tin = [0]*n # We use intime as visited if it is zero(0).
+        low = [0]*n
+        self.dfs(0, -1, graph, tin, low, bridges)
+
+        return bridges
+            
