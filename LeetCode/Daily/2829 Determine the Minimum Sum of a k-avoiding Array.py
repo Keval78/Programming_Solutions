d2e7304d385a0f196fd43ee253d9ea8fb86628a6
Keval78 <kevalpadashala@gmail.com>
2023-08-27 19:15:57 -0400

    Leetcode Daily

--- LeetCode/Daily/2829 Determine the Minimum Sum of a k-avoiding Array.py
+++ LeetCode/Daily/2829 Determine the Minimum Sum of a k-avoiding Array.py
@@ -1,3 +1,13 @@
+"""
+###### * User Profile : Keval_78 
+LinkedIn: https://www.linkedin.com/in/kevalpadsala78/
+Github: https://github.com/Keval78
+Leetcode: https://leetcode.com/Keval_78/
+"""
+
+from typing import List
+
+
 class Solution:
     def minimumSum(self, n: int, k: int) -> int:
         if k == 1:
