7c82ab0ef5a6b8f11889dde2820ebde0783c3e82
Keval78 <kevalpadashala@gmail.com>
2024-01-23 17:10:04 -0500

    CF Problemset

---
+++ codefoces/Round917/Watering an Array.py
@@ -0,0 +1,47 @@
+'''
+###### * User Profile : Keval_78
+LinkedIn: https://www.linkedin.com/in/kevalpadsala78/
+Github: https://github.com/Keval78
+Leetcode: https://leetcode.com/Keval_78/
+'''
+
+import sys
+# from os import path
+# from math import log2, floor, ceil, sqrt, pow, gcd
+# from random import random, randint, shuffle, choice
+from collections import Counter, defaultdict, deque
+# from itertools import permutations, combinations
+# from functools import reduce
+from heapq import heapify, heappop, heappush, heapreplace
+# from bisect import bisect_left, bisect_right
+
+
+def si(): return sys.stdin.readline().strip()
+def ii(): return int(si())
+def mi(ss=" "): return map(int, si().split(ss))
+def msi(ss=" "): return map(str, si().split(ss))
+def li(ss=" "): return list(mi(ss))
+
+
+sys.setrecursionlimit(10 ** 5)
+MOD, MOD2, INF = 10 ** 9 + 7, 998244353, float('inf')
+MAX = 10**5+1
+
+def solve():
+    n, k, d = mi()
+    arr = li()
+    vi = li()
+    
+    max_score = 0
+    for idx in range(min(n+5,d)):
+        max_score = max(max_score,
+                        sum([1 for i, num in enumerate(arr) if num==(i+1)]) + (d-idx-1)//2)
+        for i in range(vi[idx%k]): 
+            arr[i]+=1
+
+    print(max_score)
+
+# test = 1
+test = ii()
+for _ in range(test):
+    solve()
